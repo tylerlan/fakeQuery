3e9dbe2f29e44c1bc3889a25ca88d1014f154f75
TYLER LANGENBRUNNER <MISTERLANGO@GMAIL.COM>
2017-05-03 11:01:03 -0700

    Implemented basrebones fakeQuery for getting elements by classname and id

--- fakeQuery.js
+++ fakeQuery.js
@@ -1,7 +1,9 @@
 var $ = function(selector) {
   if (selector.startsWith('#')) {
-
+    let id = selector.substr(1);
+    return document.getElementById(id);
   } else if (selector.startsWith('.')) {
-
+    let className = selector.substr(1);
+    return document.getElementsByClassName(className);
   }
 };
